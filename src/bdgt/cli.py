e75740589f080008b402e99054e27f7decdf30d5
ssannssarr <ssantanusarkar8@gmail.com>
2026-08-09 17:21:28 +0530

    feat: implement cli and update project metadata

---
+++ src/bdgt/cli.py
@@ -0,0 +1,32 @@
+""""Import Required Dependencies"""
+from storage import (
+    load_data,
+    save_data
+)
+import asyncclick as click
+from rich.console import Console
+
+console = Console()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument(
+    'budget',
+    required=True,
+)
+def set(
+        budget: int | None
+):
+    data = load_data()
+    data['budget'] = budget
+    save_data(data)
+    console.print(load_data())
+
+
+if __name__ == "__main__":
+    cli()
