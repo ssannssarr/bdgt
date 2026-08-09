d99e8a4c3033adaa00fea3dd224e55f134bcf8da
ssannssarr <ssantanusarkar8@gmail.com>
2026-08-09 18:12:39 +0530

    feat: add async support and transaction management to CLI

--- src/bdgt/cli.py
+++ src/bdgt/cli.py
@@ -3,6 +3,8 @@ from .storage import (
     load_data,
     save_data
 )
+import asyncio
+import time
 import asyncclick as click
 from rich.console import Console
 
@@ -10,7 +12,7 @@ console = Console()
 
 
 @click.group()
-def cli():
+async def cli():
     pass
 
 
@@ -19,14 +21,47 @@ def cli():
     'budget',
     required=True,
 )
-def set(
+async def set(
         budget: int | None
 ):
     data = load_data()
     data['budget'] = budget
     save_data(data)
-    console.print(load_data())
+    console.print(f"Budget set to ₹{load_data()['budget']}")
 
+
+@cli.command()
+@click.argument('purpose')
+@click.argument('amount')
+@click.option('--spent', is_flag=True)
+@click.option('--gained', is_flag=True)
+async def add(
+        purpose: str,
+        amount: int,
+        spent,
+        gained
+):
+    if spent == gained:
+        raise click.UsageError(
+            'Choose Only one --spent or --gained'
+        )
+
+    data = load_data()
+
+    transaction = {
+        'date': time.strftime('%d/%m/%Y'),
+        'time': time.strftime("%H:%M"),
+        'purpose': purpose,
+        'amount': amount,
+        'type': 'gained' if gained else 'spent'
+    }
+
+    data['transactions'].append(transaction)
+
+    save_data(data=data)
+    console.print(
+        f"Added ₹{amount} as {'gained' if gained else 'spent'}"
+    )
 
 if __name__ == "__main__":
-    cli()
+    asyncio.run(cli())
